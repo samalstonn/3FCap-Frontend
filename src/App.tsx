5194892d66d4d9ba6e0af8a5fe7f480cdbf5698a
Sam Alston <109310905+samalstonn@users.noreply.github.com>
2023-08-08 11:57:48 -0400

    Added MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
+export async function loadData() {
+  const url = 'http://localhost:5000/portfolio'
+  return fetch(url)
+}
 function App() {
+  const [data, setData] = useState<any>()
+
+  const onDataLoaded = (data: any) => {
+    setData(data)
+  }
+  async function handleLoad(onDataLoaded: (data: any) => void) {
+    await loadData().then((response) => response.json()).then((payload) => {
+      onDataLoaded(payload)
+    })
+  }
+
+  useEffect(() => {
+    handleLoad(onDataLoaded)
+  }, [])
+
   return (
-    <p>Hello</p>
+    <div className="App">
+      <p>
+        {JSON.stringify(data)}
+      </p>
+
+
+    </div>
   )
 }
 
-export default App;
+export default App
